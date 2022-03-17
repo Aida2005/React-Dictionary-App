dab045289f7c5c4f5698c5bc04f47c70e7aefe04
Aida <aydahamedi56@gmail.com>
2022-03-17 06:45:13 -0700

    fixed the synonyms

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -5,7 +5,7 @@ export default function Synonyms(props){
    return (
        <ul className="Synonyms">
            {props.synonyms.map(function (synonym, index){
-               return <li key={index}>{synonym}</li>
+               return <ol key={index}>{synonym}</ol>
            })}
        </ul>
    )
