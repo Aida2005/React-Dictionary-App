b88ec4167aa173598cca66fd8a47b0b8af4f4f5e
Aida <aydahamedi56@gmail.com>
2022-03-18 06:29:00 -0700

    Finished the React Dictionary Project

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ export default function App() {
         <Dictionary defaultKeyword="Hello"/>
       </main>
       <footer className="App-footer">
-       Coded by Aida Hamedi
+       This project was coded by <a href="https://hungry-mirzakhani-f66424.netlify.app" target="_blank" rel="noopener noreferrer">Aida Hamedi</a> and <a href="https://github.com/shecodesio/dictionary" target="_blank" rel="noopener noreferrer"> is open-sourced on GitHub</a> and <a href="https://shecodes-dictionary.netlify.app/" target="_blank" rel="noopener noreferrer">hosted on Netlify</a>
      </footer>
       </div>
     </div>
